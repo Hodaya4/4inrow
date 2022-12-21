ef951cce926a5ebcfcf8b7a936646eccb57eaa4e
Hodaya Gamliel <Hodaya4799@gmail.com>
2022-12-21 15:03:48 +0200

    finished.

--- src/App.js
+++ src/App.js
@@ -13,13 +13,32 @@ class App extends React.Component {
             ['', '', '', '', '', '', ''],
             ['', '', '', '', '', '', '']
         ],
-        player: "y"
+        player: "y",
+        isWinner: false,
     }
 
 
+    restartGame = () => {
+        const newBoard = [
+            ['', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '']
+        ]
+
+        this.setState({
+            values: newBoard,
+            player: 'y',
+            isWinner: false
+        })
+    }
 
     cellClicked = (row, cell) => {
-/*        alert("row: " + row + " cell: " + cell);*/
+        if(this.state.isWinner) {
+            return;
+        }
         if (this.state.values[row][cell] !== '') {
             return
         }
@@ -38,8 +57,9 @@ class App extends React.Component {
 
 
     checkWin = (row, cell) => {
+
         let tempCell = cell;
-        let secondCell, thirdCell, fourthCell;
+        let secondCell, thirdCell, fourthCell = false;
         while (tempCell !== 0 && this.state.values[row][tempCell - 1] === this.state.player) {
             tempCell--
         }
@@ -48,15 +68,16 @@ class App extends React.Component {
             thirdCell = this.state.values[row][tempCell] === this.state.values[row][tempCell+2];
             fourthCell = this.state.values[row][tempCell] === this.state.values[row][tempCell+3];
             if (secondCell && thirdCell && fourthCell) {
-                alert(this.state.player + " won")
+                this.setState({
+                    isWinner: true
+                })
             }
         }
 
         tempCell = cell;
         let tempRow = row;
 
-        while (tempRow !== 0 && this.state.values[row + 1][cell] === this.state.player) {
-            console.log(tempRow)
+        while (tempRow !== 0 && this.state.values[tempRow - 1][cell] === this.state.player) {
             tempRow--
         }
         if (tempRow + 3 <= 5) {
@@ -64,9 +85,46 @@ class App extends React.Component {
             thirdCell = this.state.values[tempRow][cell] === this.state.values[tempRow + 2][cell];
             fourthCell = this.state.values[tempRow][cell] === this.state.values[tempRow + 3][cell];
             if (secondCell && thirdCell && fourthCell) {
-                alert(this.state.player + " won")
+                this.setState({
+                    isWinner: true
+                })
             }
         }
+
+        tempCell = cell;
+        tempRow = row;
+        while(tempRow !== 0 && tempCell !== 0 && this.state.values[tempRow - 1][tempCell - 1] === this.state.player) {
+            tempCell--;
+            tempRow--;
+        }
+        if(tempRow + 3 <= 5 && tempCell + 3 <= 6) {
+            secondCell = this.state.values[row][cell] === this.state.values[tempRow + 1][tempCell + 1];
+            thirdCell = this.state.values[row][cell] === this.state.values[tempRow + 2][tempCell + 2];
+            fourthCell = this.state.values[row][cell] === this.state.values[tempRow + 3][tempCell + 3];
+            if (secondCell && thirdCell && fourthCell) {
+                this.setState({
+                    isWinner: true
+                })
+            }
+
+        }
+
+        while(tempRow !== 0 && tempCell !== 6 && this.state.values[tempRow - 1][tempCell + 1] === this.state.player) {
+            tempCell++;
+            tempRow--;
+        }
+        if(tempRow + 3 <= 5 && tempCell + 3 >= 0) {
+            secondCell = this.state.values[row][cell] === this.state.values[tempRow + 1][tempCell - 1];
+            thirdCell = this.state.values[row][cell] === this.state.values[tempRow + 2][tempCell - 2];
+            fourthCell = this.state.values[row][cell] === this.state.values[tempRow + 3][tempCell - 3];
+            if (secondCell && thirdCell && fourthCell) {
+                this.setState({
+                    isWinner: true,
+                })
+            }
+        }
+
+
 
 
     }
@@ -97,6 +155,9 @@ class App extends React.Component {
                         })
                     }
                 </table>
+                <button onClick={() => this.restartGame()}>Restart Game</button>
+                <h1 className={this.state.isWinner ? "winner" : "noWinner"}>{this.state.player === 'r' ? "Yellow Won" : "Red Won"}</h1>
+
             </div>
         );
     }
